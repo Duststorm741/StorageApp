a9f1aa2f6a90556c0eeca41f77e64c2fbbfecbd5
Cole <80992301+Duststorm741@users.noreply.github.com>
2023-11-01 18:08:31 -0400

    App

---
+++ app/src/main/java/example/com/storageapplication/MainActivity.java
@@ -0,0 +1,93 @@
+package example.com.storageapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button selectFileButton;
+    private TextView selectedFileTextView;
+    private Uri selectedFileUri;
+
+    private StorageReference storageReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        selectFileButton = findViewById(R.id.selectFileButton);
+        selectedFileTextView = findViewById(R.id.selectedFileTextView);
+
+        storageReference = FirebaseStorage.getInstance().getReference(); // Initialize Firebase Storage
+
+        // Button click listener to open the file picker dialog.
+        selectFileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Open a file picker dialog to select a file.
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            // Get the selected file's URI and display it.
+            selectedFileUri = data.getData();
+            selectedFileTextView.setText("Selected File: " + selectedFileUri.getPath());
+        }
+    }
+
+    // Button click handler for sending the selected file to Firebase Cloud Storage
+    public void onClick(View view) {
+        if (selectedFileUri == null) {
+            // No file selected, exit without sending.
+            return;
+        }
+
+        // Get a reference to the location where you want to store your file in Firebase Storage
+        StorageReference fileRef = storageReference.child("Storage/" + System.currentTimeMillis());
+
+        try {
+            // Open an InputStream from the selected file and upload it to Firebase Storage.
+            InputStream stream = getContentResolver().openInputStream(selectedFileUri);
+            UploadTask uploadTask = fileRef.putStream(stream);
+
+            uploadTask.addOnSuccessListener(taskSnapshot -> {
+                Toast.makeText(getApplicationContext(), "File uploaded successfully", Toast.LENGTH_SHORT).show();
+            }).addOnFailureListener(exception -> {
+                Toast.makeText(getApplicationContext(), "Upload failed: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "File selection failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // Add this method to your MainActivity.java
+    public void goToSecondActivity(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/example/com/storageapplication/SecondActivity.java
@@ -0,0 +1,117 @@
+package example.com.storageapplication;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private ListView fileListView;
+    private Button backButton;
+
+    private ArrayAdapter<String> fileListAdapter;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        fileListView = findViewById(R.id.fileListView);
+        backButton = findViewById(R.id.backButton);
+
+        // Initialize Firebase Storage
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReferenceFromUrl("gs://serverproject-55561.appspot.com/Storage");
+
+        // Initialize the ArrayAdapter for the ListView
+        fileListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        fileListView.setAdapter(fileListAdapter);
+
+        // Load the list of files from Firebase Storage
+        loadFilesFromStorage();
+
+        // Set a click listener for the list items to download files
+        fileListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String fileName = fileListAdapter.getItem(position);
+                if (fileName != null) {
+                    downloadFile(fileName);
+                }
+            }
+        });
+
+        // Set a click listener for the back button
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Return to the Main Activity
+                finish();
+            }
+        });
+    }
+
+    private void loadFilesFromStorage() {
+        // List all files in your Firebase Storage folder
+        storageRef.listAll()
+                .addOnSuccessListener(new OnSuccessListener<ListResult>() {
+                    @Override
+                    public void onSuccess(ListResult listResult) {
+                        for (StorageReference item : listResult.getItems()) {
+                            fileListAdapter.add(item.getName());
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Handle errors here
+                        Toast.makeText(SecondActivity.this, "Failed to load files from Firebase Storage", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void downloadFile(String fileName) {
+        // Creates a reference to the selected file
+        StorageReference fileRef = storageRef.child(fileName);
+
+        // Create a local file in the Downloads folder
+        File downloadsFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File localFile = new File(downloadsFolder, fileName);
+
+        fileRef.getFile(localFile)
+                .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                        // File successfully downloaded to the Downloads folder
+                        Toast.makeText(SecondActivity.this, "File downloaded to Downloads folder", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Handle errors here
+                        Toast.makeText(SecondActivity.this, "Failed to download file", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
